608de271efa04a998f7f3960f17972bc53e2ad17
Mustafa Karakaya <mustafa.karakaya@hmcts.net>
2026-04-23 12:51:12 +0100

    CIMD-3358 Framework E Upgrade

--- documentqueue-query/documentqueue-query-view/src/test/java/uk/gov/moj/cpp/prosecution/documentqueue/query/view/service/DocumentServiceTest.java
+++ documentqueue-query/documentqueue-query-view/src/test/java/uk/gov/moj/cpp/prosecution/documentqueue/query/view/service/DocumentServiceTest.java
@@ -5,7 +5,7 @@ import static java.util.Collections.singletonList;
 import static java.util.UUID.fromString;
 import static java.util.UUID.randomUUID;
 import static java.util.stream.Collectors.toList;
-import static javax.json.Json.createObjectBuilder;
+import static uk.gov.justice.services.messaging.JsonObjects.createObjectBuilder;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
